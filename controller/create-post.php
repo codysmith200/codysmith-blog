d97d8dccf741df7b9d665f808473fc22f948799f
cody smith <codysmith200@yahoo.com>
2014-11-12 08:52:37 -0800

    video 23

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,4 +1,8 @@
 <?php
+// we required this so we can connect to the variebols 
+require_once (__dir__ . "/../model/database.php");
+// this connects us to mysqil 
+ $connection = new  mysqil($host, $username, $password, $database);
 // this will filter the input to the title
 $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
 // this will filter the input to the post 
@@ -8,3 +12,5 @@ $post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
 echo "<p>Title: $title</p>";
 // this will echo out the same thing 
 echo "<p>post: $post</p>";
+//closes connection 
+$connection->close();
