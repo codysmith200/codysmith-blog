948e2087485aa67b70d3b05574d8bb774994a2cd
cody smith <codysmith200@yahoo.com>
2014-11-13 09:15:56 -0800

    video 24

--- controller/create-post.php
+++ controller/create-post.php
@@ -2,15 +2,23 @@
 // we required this so we can connect to the variebols 
 require_once (__dir__ . "/../model/database.php");
 // this connects us to mysqil 
- $connection = new  mysqil($host, $username, $password, $database);
+ $connection = new mysqli($host, $username, $password, $database);
 // this will filter the input to the title
 $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
 // this will filter the input to the post 
 $post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
 
-//this echos out thhe title and if we resived the title 
-echo "<p>Title: $title</p>";
-// this will echo out the same thing 
-echo "<p>post: $post</p>";
+ // this connects the query to the post 
+$query = $connection->query("INSERT INTO posts SET title = '$title', post = '$post'");
+// this echos out if it seccessfully inserted the post 
+ if($query){
+     echo "<p>successfully inserted post: $title</P>";   
+} else {
+    // this echos out if we have a error
+    echo "<p>$connection->error</p>";
+}
+    
+    
+    
 //closes connection 
 $connection->close();
