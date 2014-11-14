6b1cce1e497311774f4ed631ea440ffe02fa31b4
cody smith <codysmith200@yahoo.com>
2014-11-14 08:59:39 -0800

    video 26

---
+++ model/Database.php
@@ -0,0 +1,11 @@
+<?php
+// this class will make it eser to read and cleaner 
+class Database {
+    // private is where this code is only abol to be seen in this file not out of the file 
+  private $connection; 
+  private $host;
+  private $username; 
+  private $password; 
+  private $Database;
+}
+
