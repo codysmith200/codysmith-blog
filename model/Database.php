f8a42f8b2d953ad6ff289d94aba99cb559554ba3
cody smith <codysmith200@yahoo.com>
2014-11-17 10:22:46 -0800

    video 27

--- model/Database.php
+++ model/Database.php
@@ -1,7 +1,9 @@
 <?php
-// this class will make it eser to read and cleaner 
+// this class will make it easyer to read and cleaner up the code
+// A class is a collection of variables and functions working with these variables.
+//a class is a constent of a object 
 class Database {
-    // private is where this code is only abol to be seen in this file not out of the file 
+  // private is where this code is only abel to be seen in this file it is in not out of the file 
   private $connection; 
   private $host;
   private $username; 
