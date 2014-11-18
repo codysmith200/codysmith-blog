604ed6a7a1857c6e4fa8e867d0cdb23ea30f0b33
cody smith <codysmith200@yahoo.com>
2014-11-18 08:49:07 -0800

    video 29

--- model/Database.php
+++ model/Database.php
@@ -1,33 +1,38 @@
 <?php
+
 // this class will make it easyer to read and cleaner up the code
 // A class is a collection of variables and functions working with these variables.
 //a class is a constent of a object 
 class Database {
-  // private is where this code is only abel to be seen in this file it is in not out of the file 
-  private $connection; 
-  private $host;
-  private $username; 
-  private $password; 
-  private $Database;
-  
-  public function __construct($host, $username, $password, $database) {
-      $this->host = $host; 
-      $this->username = $username;
-      $this->password = $password;
-      $this->database = $database;       
-  }
-  public function openconnection(){
-      
-      
-  }
-  public function closeconection(){
-      
-  }
-  public function query($string){
-      
-  }
-  
-  
-  
-}
 
+    // private is where this code is only abel to be seen in this file it is in not out of the file 
+    private $connection;
+    private $host;
+    private $username;
+    private $password;
+    private $Database;
+
+    public function __construct($host, $username, $password, $database) {
+        $this->host = $host;
+        $this->username = $username;
+        $this->password = $password;
+        $this->database = $database;
+    }
+
+    public function openconnection() {
+        $this->connection = new mysqli($this->host, $this->username, $this->password, $this->Database);
+        // this exits out of my web site and shows me the error 
+        if ($connection->connect_error) {
+            die("error: " . $connection->connect_error);
+        }
+    }
+
+    public function closeconection() {
+        
+    }
+
+    public function query($string) {
+        
+    }
+
+}
