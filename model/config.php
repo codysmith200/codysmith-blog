6e181c8a00fad027f594fbc58ed836a80e27dbe9
cody smith <codysmith200@yahoo.com>
2014-11-21 09:01:54 -0800

    video 35

--- model/config.php
+++ model/config.php
@@ -1,6 +1,6 @@
 <?php
 require_once(__dir__ . "/database.php");
-
+session_start();
 $path = "/codysmith-blog/";
 
 // connects to localhost and will log in for me 
@@ -9,6 +9,8 @@ $username = "root";
 $password = "root";
 $database = "blog_db";
 
+if(!isset($_SESSION["connection"])) {
 $connection = new Database($host, $username, $password, $database);
-
+$_SESSION["connection"] = $connection;
+}
 
